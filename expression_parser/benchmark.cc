f2a190f8372875458ea4388cd0cdce4fcb0c2223
Maciek Gajewski <maciej.gajewski0@gmail.com>
2019-02-11 11:31:15 +0100

    1% error case added to benchmark

--- expression_parser/benchmark.cc
+++ expression_parser/benchmark.cc
@@ -7,9 +7,11 @@
 namespace ExpressionParser {
 namespace Benchmarks {
 
-static std::vector<Expression> expressions;
+static std::vector<Expression> expressions_valid;
+static std::vector<Expression> expressions_invalid_1;
 
-static void NoExceptionsValid(benchmark::State &state) {
+static void NoExceptions(benchmark::State &state,
+                         const std::vector<Expression> &expressions) {
   std::size_t idx = 0;
   for (auto _ : state) {
     int result;
@@ -21,18 +23,26 @@ static void NoExceptionsValid(benchmark::State &state) {
   }
 }
 
-static void ExceptionsValid(benchmark::State &state) {
+static void Exceptions(benchmark::State &state,
+                       const std::vector<Expression> &expressions) {
   std::size_t idx = 0;
   for (auto _ : state) {
-    ::benchmark::DoNotOptimize(evaluateExceptions(expressions[idx].expression));
+    try {
+      ::benchmark::DoNotOptimize(
+          evaluateExceptions(expressions[idx].expression));
+    } catch (...) {
+    }
     ++idx;
     if (idx == expressions.size())
       idx = 0;
   }
 }
 
-BENCHMARK(NoExceptionsValid);
-BENCHMARK(ExceptionsValid);
+BENCHMARK_CAPTURE(NoExceptions, valid, expressions_valid);
+BENCHMARK_CAPTURE(Exceptions, valid, expressions_valid);
+
+BENCHMARK_CAPTURE(NoExceptions, invalid1, expressions_invalid_1);
+BENCHMARK_CAPTURE(Exceptions, invalid1, expressions_invalid_1);
 
 } // namespace Benchmarks
 } // namespace ExpressionParser
@@ -42,7 +52,18 @@ int main(int argc, char **argv) {
   ::benchmark::Initialize(&argc, argv);
   if (::benchmark::ReportUnrecognizedArguments(argc, argv))
     return 1;
-  ExpressionParser::Benchmarks::expressions =
+
+  // load expressions
+  ExpressionParser::Benchmarks::expressions_valid =
       ExpressionParser::loadFromFile("expressions.txt");
+
+  // create a set with 1 invalid
+  ExpressionParser::Benchmarks::expressions_invalid_1 =
+      ExpressionParser::Benchmarks::expressions_valid;
+
+  auto &expression = ExpressionParser::Benchmarks::expressions_invalid_1[7];
+  expression.expression[expression.expression.length() / 2] =
+      'x'; // spoil the expression
+
   ::benchmark::RunSpecifiedBenchmarks();
 }
