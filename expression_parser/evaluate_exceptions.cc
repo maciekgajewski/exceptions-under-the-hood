49311dd7c01aaca4a21021db544b6faf23abc691
Maciej Gajewski <maciej.gajewski0@gmail.com>
2019-02-24 12:17:29 +0100

    no more templates in parser

--- expression_parser/evaluate_exceptions.cc
+++ expression_parser/evaluate_exceptions.cc
@@ -1,1 +1,82 @@
 #include "evaluate_exceptions.hh"
+namespace ExpressionParser {
+namespace DetailExceptions {
+
+static int evaluateParenthesis(const char *&current, const char *last);
+static int evaluateNumber(const char *&current, const char *last);
+
+static int evaluate(const char *&current, const char *last) {
+  int value = 0;
+  int mply = 1;
+  while (current != last) {
+    // parse value
+
+    if (*current == '(') {
+      value += mply * evaluateParenthesis(current, last);
+    } else if (*current >= '0' && *current <= '9') {
+      value += mply * evaluateNumber(current, last);
+    } else {
+      throw std::runtime_error("Error parsing value");
+    }
+
+    // maybe parse operator
+    if (current == last || *current == ')')
+      break;
+
+    if (*current == '+')
+      mply = 1;
+    else if (*current == '-')
+      mply = -1;
+    else
+      throw std::runtime_error("Error parsing operator");
+    ++current;
+  }
+
+  return value;
+}
+
+static int evaluateParenthesis(const char *&current, const char *last) {
+
+  assert(*current == '(');
+  ++current;
+  int value = evaluate(current, last);
+  if (current == last || *current != ')')
+    throw std::runtime_error("Missing closing parenthesis");
+  ++current;
+  return value;
+}
+
+static int evaluateNumber(const char *&current, const char *last) {
+
+  assert(*current >= '0' && *current <= '9');
+
+  int result = 0;
+  while (true) {
+    result += *current - '0';
+    ++current;
+    if (current == last)
+      return result;
+    if (*current < '0' || *current > '9')
+      return result;
+    result *= 10;
+  }
+}
+
+} // namespace DetailExceptions
+
+static int evaluateExceptions(const char *first, const char *last) {
+
+  if (first == last)
+    throw std::runtime_error("Empty expression");
+  int value = DetailExceptions::evaluate(first, last);
+  if (first != last)
+    throw std::runtime_error("Unmatched ) at the end of expression");
+  return value;
+}
+
+// evaluates expression, throws on error
+int evaluateExceptions(const std::string &expression) {
+  return evaluateExceptions(expression.data(),
+                            expression.data() + expression.length());
+}
+} // namespace ExpressionParser
--- expression_parser/evaluate_no_exceptions.cc
+++ expression_parser/evaluate_no_exceptions.cc
@@ -2,4 +2,103 @@
 
 namespace ExpressionParser {
 std::string _error;
+namespace DetailNoExceptions {
+
+static bool evaluateParenthesis(const char *&current, const char *last,
+                                int &result);
+static int evaluateNumber(const char *&current, const char *last);
+
+static bool evaluate(const char *&current, const char *last, int &result) {
+  int value = 0;
+  int mply = 1;
+  while (current != last) {
+    // parse value
+
+    if (*current == '(') {
+      int res;
+      if (!evaluateParenthesis(current, last, res))
+        return false;
+      value += mply * res;
+    } else if (*current >= '0' && *current <= '9') {
+      value += mply * evaluateNumber(current, last);
+    } else {
+      _error = "Error parsing value";
+      return false;
+    }
+
+    // maybe parse operator
+    if (current == last || *current == ')')
+      break;
+
+    if (*current == '+')
+      mply = 1;
+    else if (*current == '-')
+      mply = -1;
+    else {
+      _error = "Error parsing operator";
+      return false;
+    }
+    ++current;
+  }
+
+  result = value;
+  return true;
 }
+
+static bool evaluateParenthesis(const char *&current, const char *last,
+                                int &result) {
+
+  assert(*current == '(');
+  ++current;
+  if (!evaluate(current, last, result))
+    return false;
+  if (current == last || *current != ')') {
+    _error = "Missing closing parenthesis";
+    return false;
+  }
+  ++current;
+  return true;
+}
+
+static int evaluateNumber(const char *&current, const char *last) {
+
+  assert(*current >= '0' && *current <= '9');
+
+  int result = 0;
+  while (true) {
+    result += *current - '0';
+    ++current;
+    if (current == last)
+      return result;
+    if (*current < '0' || *current > '9')
+      return result;
+    result *= 10;
+  }
+}
+
+} // namespace DetailNoExceptions
+
+static int evaluateNoExceptions(const char *first, const char *last,
+                                int &result) {
+
+  if (first == last) {
+    _error = "Empty expression";
+    return false;
+  }
+  bool res = DetailNoExceptions::evaluate(first, last, result);
+  if (res) {
+    if (first != last) {
+      _error = "Unmatched ) at the end of expression";
+      return false;
+    }
+  }
+  return res;
+}
+
+// evaluates expression, return true if ok. Value returned by out param, error
+// in global _error;
+bool evaluateNoExceptions(const std::string &expression, int &result) {
+  return evaluateNoExceptions(expression.data(),
+                              expression.data() + expression.length(), result);
+}
+} // namespace ExpressionParser
