7ec1d0faf13df44e44cad5c71e585803084d7e3e
Maciek Gajewski <maciej.gajewski0@gmail.com>
2019-03-13 11:56:37 +0100

    some cleanup with experiments

--- experiments/int2uint.cc
+++ experiments/int2uint.cc
@@ -5,22 +5,47 @@ void fun()
     throw 7u;
 }
 
-int main(int argc, char **argv)
+void catch_int_only()
 {
     try
     {
+        std::cout << "will catch 'int' and '...'" << std::endl;
         fun();
     }
     catch (int)
     {
         std::cout << "int caught" << std::endl;
-    } /*
-    catch (unsigned)
-    {
-        std::cout << "unsigned caught" << std::endl;
-    }*/
+    }
     catch (...)
     {
         std::cout << "... caught" << std::endl;
     }
+}
+
+void catch_int_unsigned()
+{
+    try
+    {
+        std::cout << "will catch 'int' and '...'" << std::endl;
+        fun();
+    }
+    catch (unsigned int)
+    {
+        std::cout << "unsigned int caught" << std::endl;
+    }
+    catch (int)
+    {
+        std::cout << "int caught" << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "... caught" << std::endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << "int2uint" << std::endl;
+    catch_int_only();
+    catch_int_unsigned();
 }
